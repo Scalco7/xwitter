e7496288bcaba402e9f4fc2c37d3de071dc30b02
Scalco7 <fscalco7@gmail.com>
2024-09-12 19:31:03 -0300

    update api service

--- lib/app/common/services/api.service.dart
+++ lib/app/common/services/api.service.dart
@@ -2,26 +2,29 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'package:xwitter/app/common/controllers/user.controller.dart';
 
 class ApiService {
   Future<Response> post(
       {required String url, required Map<String, dynamic> jsonBody}) async {
-    Map<String, String> headers = {
-      'Content-type': 'application/json',
-      'Accept': 'application/json',
-    };
-
     var body = json.encode(jsonBody);
 
-    return http.post(Uri.parse(url), body: body, headers: headers);
+    return http.post(Uri.parse(url), body: body, headers: _getHeaders());
   }
 
   Future<Response> get({required String url}) async {
+    return http.get(Uri.parse(url), headers: _getHeaders());
+  }
+
+  Map<String, String> _getHeaders() {
+    String? loggedUserId = UserController().loggedUser?.id;
     Map<String, String> headers = {
       'Content-type': 'application/json',
       'Accept': 'application/json',
     };
 
-    return http.get(Uri.parse(url), headers: headers);
+    if (loggedUserId != null) headers['loggeduserid'] = loggedUserId;
+
+    return headers;
   }
 }
