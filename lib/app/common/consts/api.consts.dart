f022714db611cf48776488b2550ca4cc6f3ca7b4
Scalco7 <fscalco7@gmail.com>
2024-09-16 14:19:57 -0300

    img mem pé

--- lib/app/common/consts/api.consts.dart
+++ lib/app/common/consts/api.consts.dart
@@ -1,3 +1,6 @@
 class ApiConsts {
+  // emulator - http://10.0.2.2:3333
+  // fidical device - http://192.168.3.15:3333
+
   static const String apiUrl = "http://10.0.2.2:3333";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:xwitter/app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(const BigTalk());
 }
