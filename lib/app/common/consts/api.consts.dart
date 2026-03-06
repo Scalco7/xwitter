0b11df86025747e27e7386044db539e4204f1549
Felipe Scalco <fscalco7@gmail.com>
2026-03-06 14:00:20 -0300

    fix: upgrade project

--- lib/app/common/consts/api.consts.dart
+++ lib/app/common/consts/api.consts.dart
@@ -2,5 +2,5 @@ class ApiConsts {
   // emulator - http://10.0.2.2:3333
   // fidical device - http://192.168.3.15:3333
 
-  static const String apiUrl = "http://10.0.2.2:3333";
+  static const String apiUrl = "https://athletic-happiness-production.up.railway.app";
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,11 +7,8 @@ import Foundation
 
 import cloud_firestore
 import file_selector_macos
-import firebase_auth
 import firebase_core
-import firebase_database
 import package_info_plus
-import path_provider_foundation
 import shared_preferences_foundation
 import video_player_avfoundation
 import wakelock_plus
@@ -19,11 +16,8 @@ import wakelock_plus
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
